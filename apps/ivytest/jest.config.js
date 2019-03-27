321db79605695aadff2241294740ba7850bf6f27
Jason Jean <jasonjean1993@gmail.com>
2019-03-27 11:15:09 -0400

    format

--- apps/ivytest/jest.config.js
+++ apps/ivytest/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: "ivytest",
-  preset: "../../jest.config.js",
-  coverageDirectory: "../../coverage/apps/ivytest/",
+  name: 'ivytest',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/ivytest/',
   snapshotSerializers: [
-    "jest-preset-angular/AngularSnapshotSerializer.js",
-    "jest-preset-angular/HTMLCommentSerializer.js"
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
   ]
 };
